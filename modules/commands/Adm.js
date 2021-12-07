241490db286c29bba33c1b7e565ff15dd96babd0
tuansu77 <77950318+tuansu77@users.noreply.github.com>
2021-12-07 13:18:46 +0700

    first commit

---
+++ modules/commands/Adm.js
@@ -0,0 +1,19 @@
+/** Mod lại module "bot.js" nếu muốn nó hiển thị ảnh **/
+/** Đổi Credit ? Bọn t đã không mã hóa cho mà edit rồi thì tôn trọng nhau tý đi ¯\_(ツ)_/¯ **/
+module.exports.config = {
+	name: "adm",
+	version: "1.0.0",
+	hasPermssion: 0,
+	credits: "HChong",
+	description: "Thông tin người điều hành bot",
+	commandCategory: "info",
+	cooldowns: 1
+};
+
+module.exports.run = ({ event, api }) => api.sendMessage(`Thông tin người điều hành bot:
+Facebook: Hoàng Long Tuấn (shenmei)
+TikTok: 
+UID: 100061220002846
+Giới tính: Men 😼😼
+Mối quan hệ: Độc thân
+Link Facebook: https://www.facebook.com/100061220002846`, event.threadID, event.messageID);
